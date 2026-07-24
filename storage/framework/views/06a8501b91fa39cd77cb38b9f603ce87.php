5b57f2d7e05dd66f5b0aa9c5e12c9b339c4c191b
simeonuba <simeonphilipuba@gmail.com>
2026-07-24 01:59:08 +0100

    Remove Laravel generated storage files from git

--- storage/framework/views/06a8501b91fa39cd77cb38b9f603ce87.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH /Users/simeonuba/Downloads/public_html (1)/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
--- storage/framework/views/1ce6ebf534e7b460380c90240d77ad00.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /Users/simeonuba/Downloads/public_html (1)/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
